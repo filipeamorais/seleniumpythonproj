434132efb22b0007fe46223c5a51c23d547b971f
Filipe Morais <afilipem@gmail.com>
2022-02-17 23:28:16 -0600

    test for opening the page and click on log in

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from selenium import webdriver
-#Creating a Firefox object
+from selenium.webdriver.common.by import By
+
+#creating a firefox object
 driver = webdriver.Firefox()
-#driver = webdriver.Chrome()
-driver.get('https://cn.bing.com/')
+
+#user credentials
+username = "afilipem@gmail.com"
+password = "y4KDvEfW5%s7"
+
+driver.get('https://www.hudl.com')
+print("Browser Opened and Page Loaded")
+
+driver.implicitly_wait(5) # seconds
+driver.find_element(By.XPATH,'//a[@href="'+"/login"+'"]').click()
+print("Clicked on Log in button and loaded log in page")
 #driver.quit()
